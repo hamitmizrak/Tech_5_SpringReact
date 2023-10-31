cba94a12ec7a74f45d347f2bb831d95b50bafde7
HamitMizrak <hamitmizrak@gmail.com>
2023-10-31 13:53:27 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/tech5/Tech5SpringReactApplication.java
@@ -0,0 +1,55 @@
+package com.hamitmizrak.tech5;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
+
+
+// Mongo aktif etmek ici
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+
+// Asenkron açmak icin
+// @EnableAsync
+
+// SCAN
+// @EntityScan(basePackages = "com.hamitmizrak.data.entity") //Entity bulamadığı zaman
+// @EnableJpaRepositories(basePackages = "com.hamitmizrak.data.repository") //Repository bulamadığı zaman
+
+// Spring Cache aktif etmek gerekiyor.
+// @EnableCaching
+
+// Auditing Aktif etmek
+//@EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
+
+// Spring Security: Şimdilik dahil etme ancak Spring security için gerekli kütüphaneleri dahil
+@SpringBootApplication(exclude = {
+		//SecurityAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+
+// @SpringBootApplication
+public class Tech5SpringReactApplication {
+
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+	}
+
+	public static void main(String[] args) {
+
+		// JOptional pane aktif etmek
+		System.setProperty("java.awt.headless","false");
+
+		// Main
+		SpringApplication.run(Tech5SpringReactApplication.class, args);
+	} //end main
+
+} //end class
---
+++ src/test/java/com/hamitmizrak/tech5/Tech5SpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.tech5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tech5SpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
