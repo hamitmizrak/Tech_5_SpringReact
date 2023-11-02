bf6cbf9dc86e5f306d39b91f3edbf80f0f93a17c
HamitMizrak <hamitmizrak@gmail.com>
2023-11-02 13:21:24 +0300

    RegisterServicesIImpl

--- src/main/java/com/hamitmizrak/tech5/business/services/impl/RegisterServicesImpl.java
+++ src/main/java/com/hamitmizrak/tech5/business/services/impl/RegisterServicesImpl.java
@@ -6,10 +6,15 @@ import com.hamitmizrak.tech5.business.dto.RegisterDto;
 import com.hamitmizrak.tech5.business.services.IRegisterServices;
 import com.hamitmizrak.tech5.data.entity.RegisterEntity;
 import com.hamitmizrak.tech5.data.repository.IRegisterRepository;
+import com.hamitmizrak.tech5.exception.HamitMizrakException;
+import com.hamitmizrak.tech5.exception.Resource404NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 // LOMBOK
 @RequiredArgsConstructor
@@ -46,7 +51,9 @@ public class RegisterServicesImpl implements IRegisterServices<RegisterDto, Regi
 
     @Override
     public String registerServiceDeleteAll() {
-        return null;
+        iRegisterRepository.deleteAll();
+        System.out.println(iRegisterRepository.findAll().toString());
+        return iRegisterRepository.findAll().toString();
     }
 
     /////////////////////////////////////////////////////////////
@@ -54,38 +61,109 @@ public class RegisterServicesImpl implements IRegisterServices<RegisterDto, Regi
     // FIND SURNAME
     @Override
     public RegisterDto loginServiceFindBySurname(String surname) {
+        Optional<RegisterEntity> find= iRegisterRepository.findByRegisterSurname(surname);
+       RegisterDto registerDto= entityToDto(find.get());
+       if(registerDto!=null){
+           return registerDto;
+       }
         return null;
     }
 
     ////////////////////////////////////////////////////////////
     // REGISTER C R U D
     // CREATE
+    // Transaction: Create, Delete, Update
+    // import org.springframework.transaction.annotation.Transactional;
     @Override
+    @Transactional
     public RegisterDto registerServiceCreate(RegisterDto registerDto) {
+        if(registerDto!=null){
+            RegisterEntity registerEntity=dtoToEntity(registerDto);
+            // PasswordEncoder (masking)
+            registerEntity.setRegisterPassword(passwordEncoderBeanClass.passwordEncoderMethod().encode(registerDto.getRegisterPassword()));
+            iRegisterRepository.save(registerEntity);
+            registerDto.setId(registerEntity.getId());
+            registerDto.setSystemDate(registerEntity.getSystemDate());
+            return registerDto;
+        }
         return null;
     }
 
     // LIST
     @Override
     public List<RegisterDto> registerServiceList() {
-        return null;
+        // Entity List
+        Iterable<RegisterEntity> registerEntityIterable= iRegisterRepository.findAll();
+        List<RegisterDto> registerDtoList=new ArrayList<>();
+        for (RegisterEntity entity : registerEntityIterable) {
+            registerDtoList.add(entityToDto(entity));
+        }
+       /*
+       log.info("Listeme: "+registerDtoList);
+        registerDtoList.stream().forEach((data)->{
+            System.out.println(data);
+        });
+        registerDtoList.forEach((data)->{
+            System.out.println(data);
+        });
+        */
+        registerDtoList.forEach(System.out::println);
+        return registerDtoList;
     }
 
     // FIND BY ID
     @Override
     public RegisterDto registerServiceFindById(Long id) {
-        return null;
+        // 1.YOL
+        /*
+         Optional<RegisterEntity>  find1=   iRegisterRepository.findById(id);
+        if(find1.isPresent()){
+            return entityToDto(find1.get());
+        }else if (id==null){
+            throw new Resource404NotFoundException(id+" nolu id yoktur");
+        }
+         */
+        RegisterEntity registerEntity=null;
+        if(id!=null){
+        RegisterEntity find2=    iRegisterRepository.findById(id)
+                .orElseThrow(()-> new Resource404NotFoundException(id+" nolu id yoktur"));
+        }else if (id==null){
+            throw new HamitMizrakException("id null olarak geldi");
+        }
+        return entityToDto(registerEntity);
     }
 
     // UPDATE
+    // Transaction: Create, Delete, Update
     @Override
+    @Transactional
     public RegisterDto registerServiceUpdate(Long id, RegisterDto registerDto) {
-        return null;
+        RegisterDto registerDtoFindDto=registerServiceFindById(id);
+        RegisterEntity  registerEntity=null;
+        if (registerDtoFindDto!=null){
+            registerEntity=dtoToEntity(registerDto);
+            registerEntity.setId(registerDto.getId());
+            registerEntity.setRegisterNickName(registerDto.getRegisterNickName());
+            registerEntity.setRegisterName(registerDto.getRegisterName());
+            registerEntity.setRegisterSurname(registerDto.getRegisterSurname());
+            registerEntity.setRegisterEmail(registerDto.getRegisterEmail());
+            registerEntity.setRegisterPassword(registerDto.getRegisterPassword());
+            registerEntity.setRegisterIsPassive(registerDto.getRegisterIsPassive());
+           iRegisterRepository.save(registerEntity);
+
+        }
+        return entityToDto(registerEntity);
     }
 
     // DELETE BY ID
+    // Transaction: Create, Delete, Update
     @Override
+    @Transactional
     public RegisterDto registerServiceDeleteById(Long id) {
-        return null;
+        RegisterDto registerDtoFindDto=registerServiceFindById(id);
+        RegisterEntity  registerEntity=null;
+        if (registerDtoFindDto!=null)
+            iRegisterRepository.deleteById(id);
+        return registerDtoFindDto;
     }
 } //end class Services
