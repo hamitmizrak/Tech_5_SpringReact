98cc157d16c68750a5385bf1db81c7cc4f8e8c8e
HamitMizrak <hamitmizrak@gmail.com>
2023-11-06 11:17:34 +0300

    @Test

---
+++ src/test/java/com/hamitmizrak/tech5/ICrudData.java
@@ -0,0 +1,21 @@
+package com.hamitmizrak.tech5;
+
+public interface ICrudData {
+
+    // C R U D
+
+    // SAVE
+    public void testCreate();
+
+    // FIND
+    public void testFindById();
+
+    // LIST
+    public void testList();
+
+    // UPDATE
+    public void testUpdate();
+
+    // DELETE
+    public void testDelete();
+}
--- src/test/java/com/hamitmizrak/tech5/Tech5SpringReactApplicationTests.java
+++ src/test/java/com/hamitmizrak/tech5/Tech5SpringReactApplicationTests.java
@@ -1,13 +1,88 @@
 package com.hamitmizrak.tech5;
 
+import com.hamitmizrak.tech5.bean.PasswordEncoderBeanClass;
+import com.hamitmizrak.tech5.data.repository.IRegisterRepository;
+import lombok.RequiredArgsConstructor;
+// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Tech5SpringReactApplicationTests {
+// LOMBOK
+@RequiredArgsConstructor
 
-	@Test
-	void contextLoads() {
+/*
+Dikkat:
+void olmalı (return olmamalı)
+metotla aynı isim olmalı
+Atomik testler olmalıdır.
+@Test annotation unutmuyoruz.
+ */
+
+// TEST
+@SpringBootTest
+class Tech5SpringReactApplicationTests implements ICrudData {
+
+	// INJECTION
+	private final IRegisterRepository iRegisterRepository;
+	private final PasswordEncoderBeanClass passwordEncoderBeanClass;
+
+	/*
+	    @Test
+		void contextLoads() {
+		}
+	*/
+	//////////////////////////////////////////////////////
+	@BeforeAll
+	static void getBeforeAllMethod(){
+		System.out.println("Bütün metotlardan önce ben çalışırım");
 	}
 
-}
+	@BeforeEach
+	void getBeforeEach(){
+		System.out.println("Testten hemen önce çalışırım.");
+	}
+
+	//////////////////////////////////////////////////////
+	// CREATE
+	@Override
+	@Test
+	public void testCreate() {
+
+	}
+
+	// FIND BY ID
+	@Override
+	@Test
+	public void testFindById() {
+
+	}
+
+	// LIST
+	@Override
+	@Test
+	public void testList() {
+
+	}
+
+	// UPDATE
+	@Override
+	@Test
+	public void testUpdate() {
+
+	}
+
+	// DELETE
+	@Override
+	@Test
+	public void testDelete() {
+
+	}
+
+	//////////////////////////////////////////////////////
+
+	//////////////////////////////////////////////////////
+
+
+} //end class
