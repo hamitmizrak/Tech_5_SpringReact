eb7698b71d110552f7e61b0ca056655ac2a06d63
HamitMizrak <hamitmizrak@gmail.com>
2023-11-08 12:40:47 +0300

    React JS template

---
+++ blog/src/RouterProject.js
@@ -0,0 +1,11 @@
+// rfce
+
+import React from 'react'
+
+function RouterProject() {
+  return (
+    <div>RouterProject</div>
+  )
+}
+
+export default RouterProject
---
+++ blog/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+
+// rcc
+import React, { Component } from 'react';
+
+// CLASS COMPONENT
+class Footer extends Component {
+
+     // Static Page Showing
+    static displayName="Footer_Blog"
+
+    // RENDER
+    render() {
+
+        // RETURN
+        return (
+            <React.Fragment>
+                FOOTER
+            </React.Fragment>
+        ); // end return
+    } // end render
+} //end Main Class
+
+// EXPORT
+export default Footer;
